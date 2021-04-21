262c63b8c25d701b99127cfacff32c300ea8a997
achiya danziger <achiya.danziger@gmail.com>
2021-04-21 23:29:32 +0300

    started tube tests

--- minipMalat_part1/src/unittests/geometries/PolygonTest.java
+++ minipMalat_part1/src/unittests/geometries/PolygonTest.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.Test;
 import geometries.*;
 import primitives.*;
 
+import java.util.List;
+
 
 /**
  * Testing Polygons
@@ -97,6 +99,37 @@ public class PolygonTest {
      */
     @Test
     void findIntersections(){
+
+        Polygon polygon= new Polygon(new Point3D(3,0,0),new Point3D(0,0,1),new Point3D(0,0,4), new Point3D(5,0,2));
+
+        // ============ Equivalence Partitions Tests ==============
+        //TC01 The Intersection with the Plane is Inside polygon
+        Point3D point=new Point3D(1,0,1);
+        List<Point3D> result = polygon.findIntersections(new Ray(new Point3D(1, -1, 1),
+                new Vector(0,1,0)));
+        assertEquals( 1, result.size(),"Wrong number of points");
+        assertEquals(point, result.get(0),"Ray crosses Polygon once");
+
+        //TC02 The Intersection with the Plane is Outside against edge
+        assertNull(polygon.findIntersections(new Ray(new Point3D(0,-1, 2),
+                new Vector(0, 1, 0))),"Ray's line out of Polygon ");
+        //TC03 The Intersection with the Plane is Outside against vertex
+        assertNull(polygon.findIntersections(new Ray(new Point3D(0,-1, 1),
+                new Vector(0, 1 ,0))),"Ray's line out of the Polygon  ");
+
+        // =============== Boundary Values Tests ==================
+
+        //TC11 the Intersection with the Plane is Outside on the edge
+        assertNull(polygon.findIntersections(new Ray(new Point3D(0,-1, 1),
+                new Vector(-1, 3,3))),"Ray's line out of the Polygon  ");
+
+        //TC12 the Intersection with the Plane is Outside In vertex
+        assertNull(polygon.findIntersections(new Ray(new Point3D(-1,-1,0),
+                new Vector(0, 1,0))),"Ray's line out of the Polygon  ");
+
+        //TC13 the Intersection with the Plane is Outside between 2 edges
+        assertNull(polygon.findIntersections(new Ray(new Point3D(5.1,-3,2),
+                new Vector(0, 1,0))),"Ray's line out of the Polygon  ");
 
     }
 
