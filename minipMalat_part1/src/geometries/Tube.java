e128fad203897e5f2945aa0110d3b33657492bcf
מלאכי מחפוד <mahpod.malachi@gmail.com>
2021-04-21 22:55:24 +0300

    tube intersections

--- minipMalat_part1/src/geometries/Tube.java
+++ minipMalat_part1/src/geometries/Tube.java
@@ -4,6 +4,7 @@ import primitives.Point3D;
 import primitives.Ray;
 import primitives.Vector;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static primitives.Util.alignZero;
@@ -65,6 +66,40 @@ public class Tube implements Geometry{
 
     @Override
     public List<Point3D> findIntersections(Ray ray) {
-        return null;
+        Vector Dp = ray.getP0().subtract(axisRay.getP0());
+
+        Vector vOnVa = axisRay.getDir().scale(ray.getDir().dotProduct(axisRay.getDir()));
+        Vector dpOnVa = axisRay.getDir().scale(Dp.dotProduct(axisRay.getDir()));
+
+        Vector a1 = vOnVa.subtract(ray.getDir());
+        Vector c1 = dpOnVa.subtract(Dp);
+
+        double A = a1.lengthSquared();
+        double B = (a1.dotProduct(c1))*2;
+        double C = c1.lengthSquared() - radius*radius;
+
+        double disc = B*B - 4*A*C;
+
+        if(disc < 0)
+            return null;
+        else if (disc == 0)
+        {
+            List<Point3D> point = new ArrayList<Point3D>();
+            double t = -B / 2*A;
+            point.add(ray.getPoint(t));
+            return point;
+        }
+        else //disc < 0
+        {
+            List<Point3D> points = new ArrayList<Point3D>();
+            disc = Math.sqrt(disc);
+            double t1 = (-B + disc) / 2*A;
+            double t2 = (-B - disc) / 2*A;
+
+            points.add(ray.getPoint(t1));
+            points.add(ray.getPoint(t2));
+
+            return points;
+        }
     }
 }
