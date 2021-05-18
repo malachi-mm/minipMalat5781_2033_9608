a70f3428ab12c0d94bc676c30b457760852094f7
מלאכי מחפוד <mahpod.malachi@gmail.com>
2021-05-18 18:49:14 +0300

    adding

--- minipMalat_part1/src/unittests/elements/RenderTests.java
+++ minipMalat_part1/src/unittests/elements/RenderTests.java
@@ -30,7 +30,7 @@ public class RenderTests {
 				.setAmbientLight(new AmbientLight(new Color(255, 191, 191), 1)) //
 				.setBackground(new Color(75, 127, 90));
 
-		scene.geometries.add(new Sphere( new Point3D(0, 0, -100),50),
+		scene.geometries.add(new Sphere(50, new Point3D(0, 0, -100)),
 				new Triangle(new Point3D(-100, 0, -100), new Point3D(0, 100, -100), new Point3D(-100, 100, -100)), // up
 																													// left
 				new Triangle(new Point3D(100, 0, -100), new Point3D(0, 100, -100), new Point3D(100, 100, -100)), // up
@@ -42,9 +42,10 @@ public class RenderTests {
 
 		ImageWriter imageWriter = new ImageWriter("base render test", 1000, 1000);
 		Render render = new Render() //
-				.setWriter(imageWriter) //
+				.setImageWriter(imageWriter) //
 				.setCamera(camera) //
 				.setRayTracer(new RayTracerBasic(scene));
+
 		render.renderImage();
 		render.printGrid(100, new Color(java.awt.Color.YELLOW));
 		render.writeToImage();
@@ -61,7 +62,7 @@ public class RenderTests {
 
 		ImageWriter imageWriter = new ImageWriter("xml render test", 1000, 1000);
 		Render render = new Render() //
-				.setWriter(imageWriter) //
+				.setImageWriter(imageWriter) //
 				.setCamera(camera) //
 				.setRayTracer(new RayTracerBasic(scene));
 
@@ -75,11 +76,12 @@ public class RenderTests {
 	 * Produce a scene with basic 3D model - including individual lights of the bodies 
 	 * and render it into a png image with a grid
 	 */
-	/*@Test
+	@Test
 	public void basicRenderMultiColorTest() {
 		Scene scene = new Scene("Test scene")//
 				.setAmbientLight(new AmbientLight(new Color(java.awt.Color.WHITE), 0.2)); //
-		scene.geometries.add(new Sphere( new Point3D(0, 0, -100),50) //
+
+		scene.geometries.add(new Sphere(50, new Point3D(0, 0, -100)) //
 				.setEmission(new Color(java.awt.Color.CYAN)), //
 				new Triangle(new Point3D(-100, 0, -100), new Point3D(0, 100, -100), new Point3D(-100, 100, -100)) // up left
 						.setEmission(new Color(java.awt.Color.GREEN)),
@@ -98,5 +100,5 @@ public class RenderTests {
 		render.renderImage();
 		render.printGrid(100, new Color(java.awt.Color.WHITE));
 		render.writeToImage();
-	}*/
+	}
 }
