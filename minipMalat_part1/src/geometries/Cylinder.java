63789c31a14d71305e8013217e5df5596434ba14
מלאכי מחפוד <mahpod.malachi@gmail.com>
2021-06-09 21:49:09 +0300

    biheth mikdash

--- minipMalat_part1/src/geometries/Cylinder.java
+++ minipMalat_part1/src/geometries/Cylinder.java
@@ -13,7 +13,7 @@ import static primitives.Util.isZero;
 /**
  * A class to represent a cylinder with Tube and the height of the cylinder
  */
-public class Cylinder extends Tube{
+public class Cylinder extends Tube {
 
     double height;
 
@@ -21,14 +21,15 @@ public class Cylinder extends Tube{
      * the main constructor of the cylinder
      * that gets everything necessary for a tube-axis ray and radius
      * with additional double for height
+     *
      * @param axisRay the axis ray
-     * @param radius the radius of the cylinder
-     * @param height the height of the cylinder
-     * @exception  IllegalArgumentException if the  radius or height are not positive
+     * @param radius  the radius of the cylinder
+     * @param height  the height of the cylinder
+     * @throws IllegalArgumentException if the  radius or height are not positive
      */
     public Cylinder(Ray axisRay, double radius, double height) {
         super(axisRay, radius);
-        if(alignZero(height) <= 0)
+        if (alignZero(height) <= 0)
             throw new IllegalArgumentException("radius must be positive");
         this.height = height;
     }
@@ -51,21 +52,19 @@ public class Cylinder extends Tube{
 
     @Override
     public Vector getNormal(Point3D point) {
-        Vector norm=axisRay.getDir();//the normal
-        Point3D upPoint=axisRay.getP0().add(axisRay.getDir().scale(height));//the center of the upper base
+        Vector norm = axisRay.getDir();//the normal
+        Point3D upPoint = axisRay.getP0().add(axisRay.getDir().scale(height));//the center of the upper base
 
         if (point.equals(axisRay.getP0())) {//if its the center of the lower base
-            norm=norm.scale(-1);
-        }
-        else if(!point.equals(upPoint)){//if it is not the center of either bases
+            norm = norm.scale(-1);
+        } else if (!point.equals(upPoint)) {//if it is not the center of either bases
 
             Vector upvec = upPoint.subtract(point);//the vector between the point and the center of the upper base
             Vector lowVec = axisRay.getP0().subtract(point);//the Vector between the point and the center of the lower base
 
-             if(isZero(lowVec.dotProduct(axisRay.getDir()))) {//if the axis ray and the lowvec are perpendicular(the point is in the lower bse)
-                norm=norm.scale(-1);//returns Vector of the axis ray scaled by -1
-            }
-            else if(!isZero(upvec.dotProduct(axisRay.getDir()))) {//if the point is not on the upper(and lower bases)
+            if (isZero(lowVec.dotProduct(axisRay.getDir()))) {//if the axis ray and the lowvec are perpendicular(the point is in the lower bse)
+                norm = norm.scale(-1);//returns Vector of the axis ray scaled by -1
+            } else if (!isZero(upvec.dotProduct(axisRay.getDir()))) {//if the point is not on the upper(and lower bases)
                 norm = super.getNormal(point);//it is the same as Tube
             }
         }
@@ -75,19 +74,35 @@ public class Cylinder extends Tube{
 
     @Override
     public List<Point3D> findIntersections(Ray ray) {
-        return null;
-    }
+        List<Point3D> points = new ArrayList<Point3D>();
 
-    @Override
-    public List<GeoPoint> findGeoIntersections(Ray ray) {
-        List<Point3D> listPoints= findIntersections(ray);
-        if(listPoints==null)
+        Point3D upPoint = axisRay.getP0().add(axisRay.getDir().scale(height));
+        List<Point3D> pointsT = super.findIntersections(ray);
+
+        if (pointsT != null)
+            for (Point3D point : pointsT) {
+                double A = point.distanceSquared(axisRay.getP0());
+                double R = radius * radius;
+                Vector v = axisRay.getP0().subtract(point);
+                if (Math.sqrt(A - R) <= height && v.dotProduct(axisRay.getDir()) <= 0)
+                    points.add(point);
+            }
+
+        Plane p1 = new Plane(axisRay.getP0(), axisRay.getDir().scale(-1));
+        Plane p2 = new Plane(upPoint, axisRay.getDir());
+
+        List<Point3D> point1 = p1.findIntersections(ray);
+        List<Point3D> point2 = p2.findIntersections(ray);
+
+        if (point1 != null && alignZero(point1.get(0).distance(axisRay.getP0()) - radius) <= 0)
+            points.add(point1.get(0));
+        if (point2 != null && alignZero(point2.get(0).distance(upPoint) - radius) <= 0)
+            points.add(point2.get(0));
+
+        if (points.isEmpty())
             return null;
-        List<GeoPoint> listGeoPoints= new ArrayList<GeoPoint>();
-
-        for (Point3D inter:listPoints)
-            listGeoPoints.add(new GeoPoint(this,inter));
-        return listGeoPoints;
+        return points;
     }
+
 
 }
